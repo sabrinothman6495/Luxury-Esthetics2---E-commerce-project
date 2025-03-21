a742a9b113c829e1e0d750a3a3bf40a1e767a253
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2025-03-21 13:57:25 -0400

    updated app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is the output directory where static files will be placed
+    outDir: '../dist', // Moves dist outside /client
+    emptyOutDir: true,
   },
+  plugins: [react()],
 });
